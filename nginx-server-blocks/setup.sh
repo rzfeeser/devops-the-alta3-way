289697a9ab0c480dd3b95ce5f9f6e665cb0b395b
seaneon <sean.r.barbour@gmail.com>
2023-05-10 11:32:18 -0400

    setup command

--- nginx-server-blocks/setup.sh
+++ nginx-server-blocks/setup.sh
@@ -3,5 +3,5 @@
 ## abandoning sites-enabled and applying a descriptive name to the server block
 sudo cp /etc/nginx/sites-enabled/default /etc/nginx/conf.d/files.conf
 
-sudo cp /home/student/git/devops-the-alta3-way/labs/base-nginx.conf
+sudo cp /home/student/git/devops-the-alta3-way/labs/base-nginx.conf /etc/nginx/nginx.conf
 
