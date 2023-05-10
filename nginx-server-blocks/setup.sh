31821a5dbe5d0543d5c58a66cd34c592ff30b9df
seaneon <sean.r.barbour@gmail.com>
2023-05-10 11:33:27 -0400

    setup command

--- nginx-server-blocks/setup.sh
+++ nginx-server-blocks/setup.sh
@@ -5,3 +5,8 @@ sudo cp /etc/nginx/sites-enabled/default /etc/nginx/conf.d/files.conf
 
 sudo cp /home/student/git/devops-the-alta3-way/labs/base-nginx.conf /etc/nginx/nginx.conf
 
+sudo nginx -t
+
+sudo systemctl restart nginx.service
+
+echo "Completed Setup"
