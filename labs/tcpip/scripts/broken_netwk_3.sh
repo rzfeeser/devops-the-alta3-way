ac564bb0fbdbd4f0ea4ccf2652e000f44cf5b208
Joe Hutcheson <96083976+HutchesonJoe@users.noreply.github.com>
2023-08-17 16:22:39 -0400

    Update broken_netwk_3.sh

--- labs/tcpip/scripts/broken_netwk_3.sh
+++ labs/tcpip/scripts/broken_netwk_3.sh
@@ -4,5 +4,5 @@ red="\033[31m"
 
 sudo ip netns exec yhost ip route del default via 10.1.2.1
 
-printf "${red}ATTENTION, networker! The network is DOWN!!! :( Better get to work!\n"
+printf "${red}ATTENTION, networker! The network is DOWN!!! :( Better get to work!\n\n"
 
