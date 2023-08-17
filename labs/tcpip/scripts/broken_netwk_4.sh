14f7b69bdf8a083264d90cfb2c38bb214c0a3be2
Joe Hutcheson <96083976+HutchesonJoe@users.noreply.github.com>
2023-08-17 16:22:08 -0400

    Update broken_netwk_4.sh

--- labs/tcpip/scripts/broken_netwk_4.sh
+++ labs/tcpip/scripts/broken_netwk_4.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+red="\033[31m"
+
+sudo ip netns exec prouter ip addr del 10.1.1.1/24 dev prout2pbrg
+sudo ip netns exec prouter ip addr add 10.1.1.1/30 dev prout2pbrg
+
+printf "${red}\nATTENTION, networker! The network is DOWN!!! :( Better get to work!\n\n"
