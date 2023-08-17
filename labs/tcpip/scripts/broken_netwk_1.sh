c85f34b82a8c2162828170a313293989eed11cd4
Joe Hutcheson <96083976+HutchesonJoe@users.noreply.github.com>
2023-08-17 13:52:51 -0400

    Update broken_netwk_1.sh

--- labs/tcpip/scripts/broken_netwk_1.sh
+++ labs/tcpip/scripts/broken_netwk_1.sh
@@ -5,4 +5,4 @@ red="\033[31m"
 sudo ip link set dev obrg2ohost down
 sudo ip link set dev obrg2orout down
 
-printf "${red}You got a busted network : ("
+printf "${red}Congratulations! You have a busted network :(\n"
